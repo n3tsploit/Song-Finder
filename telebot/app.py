9817a56095af9ee80111ed13bc91dbdb88e771cf
n3tsploit <netsploit@tutanota.com>
2022-08-06 06:24:49 +0300

    first commit

---
+++ telebot/app.py
@@ -0,0 +1,201 @@
+import json
+import time
+import schedule
+import telegram
+from telegram.ext import *
+from telegram import *
+from telebot import functions
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+import threading, redis
+
+load_dotenv(Path("./telebot/.env"))
+TOKEN = os.getenv('TOKEN')
+
+# To be used during deployemnt
+# TOKEN = os.environ.get('TOKEN')
+
+PORT = int(os.environ.get('PORT', 88))
+print('Bot is starting...')
+
+County, Area, Place = range(3)
+global county_value
+global area_value
+global regions
+
+
+# schedules the check_updates funtion in funtions.py  to be run periodically
+def update_schedule():
+    schedule.every().monday.do(functions.check_updates)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+# This function is used in the python telegram bot to handle conversions
+def start_command(update, context):
+    update.message.reply_text(f'Greetings {update.message.from_user.first_name}\U0001F601\n\nThis is an unofficial bot '
+                              f'which shows you Planned Power Interruptions in 🇰🇪.\n\nPS:Built for educational '
+                              f'purposes and it might contain outdated/erroneous information.\n\nCommands are as '
+                              f'follows:- '
+                              f'\n\U0001F4A1/check - To check the areas Planned for '
+                              f'Interruptions.\n\U0001F4A1/pdf - To download a pdf of areas listed for Planned '
+                              f'Interruptions.\n\U0001F4A1/info - To get information about the bot.'
+                              f'\n\U0001F4A1/stop - To exit the conversion.')
+
+
+# This function is used in the python telegram bot to handle conversions
+def check_command(update, context):
+    inline_keyboard = [[InlineKeyboardButton(text="Baringo", callback_data="Baringo"),
+                        InlineKeyboardButton(text="Bomet", callback_data="Bomet"),
+                        InlineKeyboardButton(text="Bungoma", callback_data="Bungoma")],
+                       [InlineKeyboardButton(text="Busia", callback_data="Busia"),
+                        InlineKeyboardButton(text="Elgeyo-Marakwet", callback_data="Elgeyo Marakwet"),
+                        InlineKeyboardButton(text="Embu", callback_data="Embu")], [
+                           InlineKeyboardButton(text="Garissa", callback_data="Garissa"),
+                           InlineKeyboardButton(text="HomaBay", callback_data="HomaBay"),
+                           InlineKeyboardButton(text="Isiolo", callback_data="Isiolo")], [
+                           InlineKeyboardButton(text="Kajiado", callback_data="Kajiado"),
+                           InlineKeyboardButton(text="Kakamega", callback_data="Kakamega"),
+                           InlineKeyboardButton(text="Kericho", callback_data="Kericho")], [
+                           InlineKeyboardButton(text="Kiambu", callback_data="Kiambu"),
+                           InlineKeyboardButton(text="Kilifi", callback_data="Kilifi"),
+                           InlineKeyboardButton(text="Kirinyaga", callback_data="Kirinyaga")], [
+                           InlineKeyboardButton(text="Kisii", callback_data="Kisii"),
+                           InlineKeyboardButton(text="Kisumu", callback_data="Kisumu"),
+                           InlineKeyboardButton(text="Kitui", callback_data="Kitui")], [
+                           InlineKeyboardButton(text="Kwale", callback_data="Kwale"),
+                           InlineKeyboardButton(text="Laikipia", callback_data="Laikipia"),
+                           InlineKeyboardButton(text="Lamu", callback_data="Lamu")], [
+                           InlineKeyboardButton(text="Machakos", callback_data="Machakos"),
+                           InlineKeyboardButton(text="Makueni", callback_data="Makueni"),
+                           InlineKeyboardButton(text="Mandera", callback_data="Mandera")], [
+                           InlineKeyboardButton(text="Marsabit", callback_data="Marsabit"),
+                           InlineKeyboardButton(text="Meru", callback_data="Meru"),
+                           InlineKeyboardButton(text="Migori", callback_data="Migori")], [
+                           InlineKeyboardButton(text="Mombasa", callback_data="Mombasa"),
+                           InlineKeyboardButton(text="Murang’a", callback_data="Murang’a"),
+                           InlineKeyboardButton(text="Nairobi", callback_data="Nairobi")], [
+                           InlineKeyboardButton(text="Nakuru", callback_data="Nakuru"),
+                           InlineKeyboardButton(text="Nandi", callback_data="Nandi"),
+                           InlineKeyboardButton(text="Narok", callback_data="Narok")], [
+                           InlineKeyboardButton(text="Nyamira", callback_data="Nyamira"),
+                           InlineKeyboardButton(text="Nyandarua", callback_data="Nyandarua"),
+                           InlineKeyboardButton(text="Nyeri", callback_data="Nyeri")], [
+                           InlineKeyboardButton(text="Samburu", callback_data="Samburu"),
+                           InlineKeyboardButton(text="Siaya", callback_data="Siaya"),
+                           InlineKeyboardButton(text="Taita-Taveta", callback_data="Taita Taveta")], [
+                           InlineKeyboardButton(text="TanaRiver", callback_data="Tana River"),
+                           InlineKeyboardButton(text="Tharaka-Nithi", callback_data="Tharaka Nithi"),
+                           InlineKeyboardButton(text="TransNzoia", callback_data="Trans Nzoia")], [
+                           InlineKeyboardButton(text="Turkana", callback_data="Turkana"),
+                           InlineKeyboardButton(text="UasinGishu", callback_data="Uasin Gishu"),
+                           InlineKeyboardButton(text="Vihiga", callback_data="Vihiga")], [
+                           InlineKeyboardButton(text="Wajir", callback_data="Wajir"),
+                           InlineKeyboardButton(text="WestPokot", callback_data="WestPokot")]]
+
+    reply_keyboard_markup = InlineKeyboardMarkup(inline_keyboard, one_time_keyboard=True, resize_keyboard=True)
+    update.message.reply_text("Please select your County:",
+                              reply_markup=reply_keyboard_markup)
+    return Area
+
+
+# This function is used in the python telegram bot to handle conversions
+def area(update, context):
+    global county_value
+    global regions
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    conn = redis.Redis(connection_pool=pool)
+    regions_json = conn.get('regions')
+    regions = json.loads(regions_json)
+    query = update.callback_query
+    query.answer()
+    county_value = query.data
+    data = functions.area_list(county=county_value, regions=regions)
+    if data is None:
+        query.edit_message_text('No Planned Power Interruptions in this County!\nExiting...')
+        return ConversationHandler.END
+    inline_keyboard = []
+    for i in data:
+        inline_keyboard.append(InlineKeyboardButton(text=str(i), callback_data=str(i)))
+
+    reply_keyboard_markup = InlineKeyboardMarkup([inline_keyboard[i:i + 1] for i in range(0, len(inline_keyboard), 1)])
+    query.edit_message_text('--fetching--')
+    query.edit_message_text(text="Select your area if it is listed below to get more details.\n"
+                                 "If not,then there isn't Planned Interruptions in your area.Click /stop to exit.",
+                            reply_markup=reply_keyboard_markup)
+    return Place
+
+
+# This function is used in the python telegram bot to handle conversions
+def place(update, context):
+    query = update.callback_query
+    query.answer()
+    place_value, time_value = functions.place_list(area=query.data, county=county_value, regions=regions)
+    place_value = '\n▪️'.join(place_value)
+    place_value = '<b>📅' + time_value + '\n' + '-' * 49 + '\n' + 'Specific places to be affected are:</b>\n▪️' + place_value
+    query.edit_message_text('--fetching--')
+    query.edit_message_text(place_value, parse_mode=telegram.ParseMode.HTML)
+
+    context.bot.sendMessage(text='Bye👋', chat_id=update.effective_chat.id)
+    return ConversationHandler.END
+
+
+# This function is used in the python telegram bot to handle conversions
+def stop(update, context):
+    update.message.reply_text('Bye..see you later👋')
+
+    return ConversationHandler.END
+
+
+# This function is used in the python telegram bot to handle conversions
+def pdf_command(update, context):
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    conn = redis.Redis(connection_pool=pool)
+    pdf_name = conn.get('pdf_name')
+    update.message.reply_text('--fetching--')
+    pdf_file = open(f'telebot/content/{pdf_name}', 'rb')
+    chat_id = update.effective_chat.id
+    return context.bot.send_document(chat_id, pdf_file)
+
+
+# This function is used in the python telegram bot to handle conversions
+def unknown(update, context):
+    update.message.reply_text('Sorry I cannot understand the text!🥲.')
+
+
+def main():
+    # This thread runs the schulder which checks for updates in the kplc website
+    thread_obj = threading.Thread(target=update_schedule)
+    thread_obj.start()
+
+    # Code from here onwards is used to run the python telegram bot
+    updater = Updater(TOKEN, use_context=True)
+    disp = updater.dispatcher
+
+    disp.add_handler(CommandHandler('info', start_command))
+    disp.add_handler(CommandHandler('start', start_command))
+    disp.add_handler(CommandHandler('pdf', pdf_command))
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('check', check_command)],
+        states={
+            Area: [CallbackQueryHandler(area)],
+            Place: [CallbackQueryHandler(place)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    disp.add_handler(conv_handler)
+
+    disp.add_handler(MessageHandler(Filters.text, unknown))
+
+    updater.start_polling()
+
+    # To be used during deployemnt
+    # updater.start_webhook(listen="0.0.0.0",
+    #                       port=int(PORT),
+    #                       url_path=TOKEN,
+    #                       webhook_url='https://powerinterruption.herokuapp.com/' + TOKEN)
+
+    updater.idle()
---
+++ telebot/functions.py
@@ -0,0 +1,172 @@
+import codecs
+import json
+import os
+import re
+import time
+from datetime import datetime
+from pathlib import Path
+import redis
+import bs4
+import requests
+import textract
+
+
+# This function checks the kplc website for the latest pdf on planned power interruptions and downloads it.
+def parse_content():
+    os.makedirs('telebot/content/', exist_ok=True)
+    res = requests.get('https://kplc.co.ke/category/view/50/planned-power-interruptions')
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    links = soup.select('.items .intro li a')
+    if not links:
+        print('No content Found')
+    else:
+        dates = []
+        for link in links:
+            file_name = os.path.basename(link.get('href'))
+            date_pattern = re.compile(r"""^(.*?)
+                ((0|1|2|3)?\d)\.
+                ((0|1)?\d)\.
+                ((19|20)\d\d)
+                (.*?)$
+                """, re.VERBOSE)
+            mo = date_pattern.search(file_name)
+            day_part = mo.group(2)
+            month_part = mo.group(4)
+            year_part = mo.group(6)
+            send = day_part + '.' + month_part + '.' + year_part
+            dates.append(send)
+        latest_date = max(dates, key=lambda d: datetime.strptime(d, '%d.%m.%Y'))
+        pattern_latest_date = re.compile(rf"{latest_date}", re.IGNORECASE)
+        for link in links:
+            if pattern_latest_date.search(str(os.path.basename(link.get('href')))):
+                url = link.get('href')
+                break
+        res = requests.get(url)
+        res.raise_for_status()
+        p = Path('telebot/content/')
+        print(list(p.glob('*')))
+        pdf_name = str(os.path.basename(url))
+        if len(list(p.glob('*.pdf'))) > 0:
+            os.remove(list(p.glob('*.pdf'))[0])
+        with open('telebot/content/' + pdf_name, 'wb') as r:
+            r.write(res.content)
+        pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+        conn = redis.Redis(connection_pool=pool)
+        conn.set('pdf_name',pdf_name)
+        return pdf_name
+
+
+# This function extracts content from the pdf and saves it to a text file
+def extract_pdf(pdf_name):
+    os.makedirs('telebot/content', exist_ok=True)
+    textract_text = textract.process(f'telebot/content/{pdf_name}')
+    textract_str_text = codecs.decode(textract_text)
+    with open(f'telebot/content/extracted_data.txt', 'w') as f:
+        f.write(textract_str_text.strip('\n'))
+
+
+# This function removes irrelevant data from the text file created above and saves it to another text file
+def clean_extracted_data():
+    pattern1_a = re.compile(r"Interruption of", re.IGNORECASE)
+    pattern1_b = re.compile(r"etc\.\)", re.IGNORECASE)
+    pattern2_a = re.compile(r"For further", re.IGNORECASE)
+    pattern2_b = re.compile(r"www\.kplc\.co\.ke", re.IGNORECASE)
+    flag = True
+    with open('telebot/content/cleaned_data.txt', 'w'):
+        pass
+
+    with open(r'telebot/content/extracted_data.txt', 'r', encoding='utf-8') as myfile:
+        for line in myfile:
+            if pattern1_a.search(line) or pattern2_a.search(line):
+                flag = False
+            if flag and line.strip():
+                with open('telebot/content/cleaned_data.txt', 'a') as f:
+                    f.write(line)
+            if pattern1_b.search(line) or pattern2_b.search(line):
+                flag = True
+
+
+# Reads data from the text file created above and forms a nested dictionary of it and stores it in a shelve binary.
+def save_data_to_shelve():
+    regions = {}
+    region_regex = re.compile(r"region", re.IGNORECASE)
+    area_regex = re.compile(r"area:", re.IGNORECASE)
+    county_regex = re.compile(r"county", re.IGNORECASE)
+    previous_line = ''
+    with open('telebot/content/cleaned_data.txt', 'r') as r:
+        for line in r.readlines():
+            if region_regex.search(line):
+                region_name = " ".join(line.split()[:-1])
+                county_name = ''
+                continue
+            if county_regex.search(line) and not 'offices' in line.casefold():
+                county_name = line.casefold().replace('parts of', '')
+                county_name = ' '.join(county_name.split()[:-1]).upper()
+                continue
+            if area_regex.search(line):
+                when = ''
+                where = ''
+                area_name = ''.join(line.split(':')[1:]).strip().lower()
+                if county_name == '':
+                    county_name = region_name
+                continue
+            if 'date:' in line.casefold() or 'time:' in line.casefold():
+                when += line.strip()
+                continue
+            where += line
+            regions.setdefault(region_name, {})
+            regions[region_name].setdefault(county_name, {})
+            regions[region_name][county_name].setdefault(area_name, {'when': '', 'where': ''})
+            if regions[region_name][county_name][area_name]['when'] == '':
+                regions[region_name][county_name][area_name]['when'] = ''.join(when).lower()
+            regions[region_name][county_name][area_name]['where'] += line.strip().lower()
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    conn = redis.Redis(connection_pool=pool)
+    regions_json = json.dumps(regions)
+    conn.set('regions', regions_json)
+
+
+# If a county has power interruptions planned, it returns areas in the specified county that will be affected.
+def area_list(county, regions):
+    for region in regions.keys():
+        if county.strip().upper() in regions[region].keys():
+            print(county)
+            print(region)
+            areas = [area for area in regions[region][county.strip().upper()].keys()]
+            print(areas)
+            if '' in areas:
+                areas.remove('')
+            return areas
+        else:
+            continue
+    return None
+
+
+# returns places in the specific area passed that will be affected by the power interruption.
+def place_list(county, area, regions):
+    county = county.strip().upper()
+    for region in regions.keys():
+        if county in regions[region].keys():
+            time_outage = regions[region][county][area]['when'].upper()
+            time_outage = re.sub('(\s\s\s)+', '', time_outage)
+            time_outage = '\n⏱️TIME'.join(time_outage.split('TIME'))
+            place_outage = [place.strip().capitalize() for place in
+                            ''.join(regions[region][county][area]['where']).split(',')]
+            return place_outage, time_outage
+
+
+# this funtion is run periodically -- refer to the app.py file
+def check_updates():
+    print('checking')
+    pdf_name = parse_content()
+    time.sleep(10)
+    extract_pdf(pdf_name)
+    time.sleep(3)
+    clean_extracted_data()
+    time.sleep(3)
+    save_data_to_shelve()
+
+    print(f'Checked for updates on {datetime.today().strftime("%Y/%m/%d %H:%M:%S")}')
