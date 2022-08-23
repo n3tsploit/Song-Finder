43596a8aef53e60c9fea0e619b0d59775b6fa37a
n3tsploit <netsploit@tutanota.com>
2022-08-23 16:43:45 +0300

    hello

--- telebot/app.py
+++
@@ -1,8 +0,0 @@
-import requests
-data = {
-    'api_token': 'ecce5a249ce9ea37615b5da1249d1d5f',
-    'url': '/home/digzsudi/Music/example.mp3',
-    'return': 'apple_music,spotify',
-}
-result = requests.post('https://api.audd.io/', data=data)
-print(result.text)
