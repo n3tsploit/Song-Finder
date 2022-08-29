9abd29e753f30effdad30cf6b983e8796aa0ba0c
n3tsploit <netsploit@tutanota.com>
2022-08-29 14:35:17 +0300

    edit

---
+++ extractAudio.py
@@ -0,0 +1,12 @@
+# Import everything needed to edit video clips
+from moviepy.editor import *
+
+# loading video dsa gfg intro video
+music = AudioFileClip("./songs/yt1s.com - Burna Boy  For My Hand feat Ed Sheeran Official Music Video.mp3")
+
+# clipping of the video
+# getting video for only starting 10 seconds
+music = music.subclip(0, 10)
+
+
+music.write_audiofile("testing.mp3",fps=44100,nbytes=2)
--- run.py
+++ run.py
@@ -1,1 +1,23 @@
+import base64
+import pprint
+import moviepy
+import requests
 
+url = "https://shazam.p.rapidapi.com/songs/v2/detect"
+
+querystring = {"timezone": "America/Chicago", "locale": "en-US"}
+
+with open('./testing.mp3', 'rb') as f:
+    content = base64.b64encode(f.read())
+payload = content
+headers = {
+    "content-type": "text/plain",
+    "X-RapidAPI-Key": "3c1dacced4msh6e95e085f64394ap1ae14fjsn691e92cf4ba5",
+    "X-RapidAPI-Host": "shazam.p.rapidapi.com"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+pprint.pprint(response.text)
+
+# how to change audio files to raw in python
