d0bf05337d6117d83f6c4f457943dc72645fd03c
n3tsploit <netsploit@tutanota.com>
2022-09-17 21:36:50 +0300

    downloading video from twitter

--- download_video.py
+++ download_video.py
@@ -9,6 +9,7 @@ consumer_secret = os.getenv('CONSUMER_SECRET')
 access_token = os.getenv('ACCESS_KEY')
 access_secret = os.getenv('ACCESS_SECRET')
 
+
 client = tweepy.Client(consumer_key= consumer_key,consumer_secret= consumer_secret,access_token= access_token,access_token_secret= access_secret)
 
 tweets = client.get_home_timeline(media_fields=['preview_image_url'], expansions='attachments.media_keys',
