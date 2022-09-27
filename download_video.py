e78204eb560b2dc860bba893438e6c08ccd4e280
n3tsploit <netsploit@tutanota.com>
2022-09-27 16:23:37 +0300

    getting twitter media url

--- download_video.py
+++ download_video.py
@@ -13,31 +13,8 @@ access_token = os.getenv('ACCESS_KEY')
 access_secret = os.getenv('ACCESS_SECRET')
 bearer_token = os.getenv('BEARER_TOKEN')
 
-client = tweepy.Client(
-    consumer_key=consumer_key, consumer_secret=consumer_secret,
-    access_token=access_token, access_token_secret=access_secret, bearer_token=bearer_token
-)
 
-query = 'usiu has:media'
+response = requests.get(url='https://api.twitter.com/2/tweets?ids=1571483067461603330&expansions=attachments.media_keys&media.fields=type,variants', headers={'Authorization': f'Bearer {bearer_token}'})
+data = response.json()
 
-
-
-response = client.get_tweet(id='1571483067461603330', expansions='attachments.media_keys,author_id', media_fields='preview_image_url,alt_text,url,variants',tweet_fields='attachments,context_annotations,id,entities',poll_fields='id',user_fields='entities,url')
-# users = {u['id']:u for u in response.includes['users']}
-# medias = {m['media_key']:m for m in response.includes['media']}
-
-# pprint.pprint(medias)
-pprint.pprint(response)
-
-# for tweet in response.data:
-#     if users[tweet.author_id]:
-#         user = users[tweet.author_id]
-#         print(tweet.entities)
-
-# for tweet in response.data:
-#     try:
-#         if medias[tweet.attachments['media_keys'][0]]:
-#             mediam = medias[tweet.attachments['media_keys'][0]]
-#             print(mediam.media_key)
-#     except:
-#         pass
+print(data)
