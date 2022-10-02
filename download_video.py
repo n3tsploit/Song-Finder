44e8d6c5287d58608c0b37a4c34933176566cc62
n3tsploit <netsploit@tutanota.com>
2022-10-02 15:28:32 +0300

    getting twitter media url

--- download_video.py
+++ download_video.py
@@ -13,10 +13,14 @@ consumer_secret = os.getenv('CONSUMER_SECRET')
 access_token = os.getenv('ACCESS_KEY')
 access_secret = os.getenv('ACCESS_SECRET')
 bearer_token = os.getenv('BEARER_TOKEN')
+user_id = os.getenv('USER_ID')
 
+response = requests.get(url=f'https://api.twitter.com/2/users/{user_id}/mentions?expansions=attachments.media_keys&media.fields=type,variants', headers={'Authorization': f'Bearer {bearer_token}'})
+id = response.json()['data'][0]['id']
 
-response = requests.get(url='https://api.twitter.com/2/tweets?ids=1571483067461603330&expansions=attachments.media_keys&media.fields=type,variants', headers={'Authorization': f'Bearer {bearer_token}'})
-data = response.json()
-url = data['includes']['media'][0]['variants'][0]['url']
+response1 = requests.get(url=f'https://api.twitter.com/2/tweets?ids={id}&expansions=attachments.media_keys&media.fields=type,variants', headers={'Authorization': f'Bearer {bearer_token}'})
+data = response1.json()
+# url = data['includes']['media'][0]['variants'][0]['url']
 
-file = wget.download(url)
+print(data)
+# file = wget.download(url)
